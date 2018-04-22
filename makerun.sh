605d86734d8b143cd0ae42dfc7515fb380bd2f3c
crypt0n1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-04-21 23:19:39 -0400

    Update makerun.sh
    Modified script for Social Wallet WIRE Masternode

--- makerun.sh
+++ makerun.sh
@@ -2,11 +2,11 @@
 # makerun.sh
 # Make sure smartcashd is always running.
 # Add the following to the crontab (i.e. crontab -e)
-# */5 * * * * ~/smartnode/makerun.sh
+# */5 * * * * ~/wirenode/makerun.sh
 
-if ps -A | grep smartcashd > /dev/null
+if ps -A | grep wired > /dev/null
 then
   exit
 else
-  smartcashd &
+  ./wired -daemon &
 fi
