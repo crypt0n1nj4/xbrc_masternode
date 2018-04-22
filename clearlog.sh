fc6dcdd13905eaebebbd69788f1c219569af4bb5
crypt0n1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-04-21 23:20:21 -0400

    Update clearlog.sh
    Modified script for Social Wallet WIRE Masternode

--- clearlog.sh
+++ clearlog.sh
@@ -2,6 +2,6 @@
 # clearlog.sh
 # Clear debug.log every other day
 # Add the following to the crontab (i.e. crontab -e)
-# 0 0 */2 * * ~/smartnode/clearlog.sh
+# 0 0 */2 * * ~/wirenode/clearlog.sh
 
-/bin/date > ~/.smartcash/debug.log
+/bin/date > ~/.wire/debug.log
