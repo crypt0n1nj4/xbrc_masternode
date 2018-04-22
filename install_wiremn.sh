96d54b1ecae49f401b28e802e934727152cb0650
crypt0n1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-04-21 23:16:25 -0400

    Update and rename install.sh to install_wiremn.sh
    Modified install script for Social Wallet WIRE Masternode

--- install_wiremn.sh
+++ install_wiremn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# install.sh
+# install_wiremn.sh
 # Installs smartnode on Ubuntu 16.04 LTS x64
 # ATTENTION: The anti-ddos part will disable http, https and dns ports.
 
@@ -9,13 +9,13 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 while true; do
- if [ -d ~/.smartcash ]; then
-   printf "~/.smartcash/ already exists! The installer will delete this folder. Continue anyway?(Y/n)"
+ if [ -d ~/.wire ]; then
+   printf "~/.wire/ already exists! The installer will delete this folder. Continue anyway?(Y/n)"
    read REPLY
    if [ ${REPLY} == "Y" ]; then
-      pID=$(ps -ef | grep smartcashd | awk '{print $2}')
+      pID=$(ps -ef | grep wired | awk '{print $2}')
       kill ${pID}
-      rm -rf ~/.smartcash/
+      rm -rf ~/.wire/
       break
    else
       if [ ${REPLY} == "n" ]; then
@@ -33,13 +33,9 @@ printf "Press Ctrl+C to cancel or Enter to continue: "
 read IGNORE
 
 cd
-# Changing the SSH Port to a custom number is a good security measure against DDOS attacks
-printf "Custom SSH Port(Enter to ignore): "
-read VARIABLE
-_sshPortNumber=${VARIABLE:-22}
 
 # Get a new privatekey by going to console >> debug and typing smartnode genkey
-printf "SmartNode GenKey: "
+printf "WIRE MN GenKey: "
 read _nodePrivateKey
 
 # The RPC node will only accept connections from your localhost
@@ -52,11 +48,11 @@ _rpcPassword=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 32 ; echo '')
 _nodeIpAddress=$(ip route get 1 | awk '{print $NF;exit}')
 
 # Make a new directory for smartcash daemon
-mkdir ~/.smartcash/
-touch ~/.smartcash/smartcash.conf
+mkdir ~/.wire/
+touch ~/.wire/wire.conf
 
 # Change the directory to ~/.smartcash
-cd ~/.smartcash/
+cd ~/.wire/
 
 # Create the initial smartcash.conf file
 echo "rpcuser=${_rpcUserName}
@@ -69,52 +65,53 @@ logtimestamps=1
 maxconnections=64
 txindex=1
 smartnode=1
-externalip=${_nodeIpAddress}:9678
+externalip=${_nodeIpAddress}:6520
 smartnodeprivkey=${_nodePrivateKey}
 " > smartcash.conf
 cd
 
 # Install smartcashd using apt-get
+apt-get install -y pkg-config
+apt-get -y install build-essential autoconf automake libtool libboost-all-dev libgmp-dev libssl-dev libcurl4-openssl-dev git 
+add-apt-repository ppa:bitcoin/bitcoin -y
 apt-get update -y
-apt-get install software-properties-common -y
-add-apt-repository ppa:smartcash/ppa -y && apt update -y && apt install smartcashd -y && smartcashd
+apt-get install libdb4.8-dev libdb4.8++-dev -y
+
+# Download the WIRE MN LINUX Daemon and CLI
+wget https://raw.githubusercontent.com/crypt0n1nj4/social_wallet_masternode_wire/master/wired
+wget https://raw.githubusercontent.com/crypt0n1nj4/social_wallet_masternode_wire/master/wire-cli
 
 # Create a directory for smartnode's cronjobs and the anti-ddos script
-rm -r smartnode
-mkdir smartnode
+rm -r wirenode
+mkdir wirenode
 
 # Change the directory to ~/smartnode/
-cd ~/smartnode/
+cd ~/wirenode/
 
 # Download the appropriate scripts
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/makerun.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/checkdaemon.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/upgrade.sh
-wget https://raw.githubusercontent.com/SmartCash/smartnode/master/clearlog.sh
+wget https://raw.githubusercontent.com/crypt0n1nj4/wire_masternode/master/makerun.sh
+wget https://raw.githubusercontent.com/crypt0n1nj4/wire_masternode/master/checkdaemon.sh
+wget https://raw.githubusercontent.com/crypt0n1nj4/wire_masternode/master/clearlog.sh
+
 
 # Create a cronjob for making sure smartcashd runs after reboot
-if ! crontab -l | grep "@reboot smartcashd"; then
-  (crontab -l ; echo "@reboot smartcashd") | crontab -
+if ! crontab -l | grep "@reboot ./wired -daemon"; then
+  (crontab -l ; echo "@reboot ./wired -daemon") | crontab -
 fi
 
 # Create a cronjob for making sure smartcashd is always running
-if ! crontab -l | grep "~/smartnode/makerun.sh"; then
-  (crontab -l ; echo "*/5 * * * * ~/smartnode/makerun.sh") | crontab -
+if ! crontab -l | grep "~/wirenode/makerun.sh"; then
+  (crontab -l ; echo "*/5 * * * * ~/wirenode/makerun.sh") | crontab -
 fi
 
 # Create a cronjob for making sure the daemon is never stuck
-if ! crontab -l | grep "~/smartnode/checkdaemon.sh"; then
-  (crontab -l ; echo "*/30 * * * * ~/smartnode/checkdaemon.sh") | crontab -
-fi
-
-# Create a cronjob for making sure smartcashd is always up-to-date
-if ! crontab -l | grep "~/smartnode/upgrade.sh"; then
-  (crontab -l ; echo "0 0 */1 * * ~/smartnode/upgrade.sh") | crontab -
+if ! crontab -l | grep "~/wirenode/checkdaemon.sh"; then
+  (crontab -l ; echo "*/30 * * * * ~/wirenode/checkdaemon.sh") | crontab -
 fi
 
 # Create a cronjob for clearing the log file
-if ! crontab -l | grep "~/smartnode/clearlog.sh"; then
-  (crontab -l ; echo "0 0 */2 * * ~/smartnode/clearlog.sh") | crontab -
+if ! crontab -l | grep "~/wirenode/clearlog.sh"; then
+  (crontab -l ; echo "0 0 */2 * * ~/wirenode/clearlog.sh") | crontab -
 fi
 
 # Give execute permission to the cron scripts
@@ -123,15 +120,12 @@ chmod 0700 ./checkdaemon.sh
 chmod 0700 ./upgrade.sh
 chmod 0700 ./clearlog.sh
 
-# Change the SSH port
-sed -i "s/[#]\{0,1\}[ ]\{0,1\}Port [0-9]\{2,\}/Port ${_sshPortNumber}/g" /etc/ssh/sshd_config
-
 # Firewall security measures
 apt install ufw -y
 ufw disable
-ufw allow 9678
-ufw allow "$_sshPortNumber"/tcp
-ufw limit "$_sshPortNumber"/tcp
+ufw allow 6520
+ufw allow 22/tcp
+ufw limit 22/tcp
 ufw logging on
 ufw default deny incoming
 ufw default allow outgoing
