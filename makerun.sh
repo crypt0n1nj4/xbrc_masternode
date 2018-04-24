1264b8246ee2501e76ac7e3e399470f049205f06
crypt0n1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-04-23 22:38:22 -0400

    Update makerun.sh

--- makerun.sh
+++ makerun.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # makerun.sh
-# Make sure smartcashd is always running.
+# Make sure bitrewardsd is always running.
 # Add the following to the crontab (i.e. crontab -e)
-# */5 * * * * ~/wirenode/makerun.sh
+# */5 * * * * ~/xbrnode/makerun.sh
 
-if ps -A | grep wired > /dev/null
+if ps -A | grep bitrewardsd > /dev/null
 then
   exit
 else
-  ./wired -daemon &
+  ./bitrewardsd -daemon &
 fi
