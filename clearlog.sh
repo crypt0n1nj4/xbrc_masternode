db778ec32ac17b215e4062c8581d58e88f7a766e
Crypt0 N1nj4 <38593912+crypt0n1nj4@users.noreply.github.com>
2018-11-21 16:58:36 -0500

    Delete clearlog.sh

--- clearlog.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# clearlog.sh
-# Clear debug.log every other day
-# Add the following to the crontab (i.e. crontab -e)
-# 0 0 */2 * * ~/xbrnode/clearlog.sh
-
-/bin/date > ~/.xbitrewards/debug.log
